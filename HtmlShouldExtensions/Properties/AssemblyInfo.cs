dd2531b0ecc83bbc179bf0d42c07fa48b6b30b0f
Simon Jefford <simon.jefford@gmail.com>
2012-07-06 14:32:48 +0100

    Added nuspec

--- HtmlShouldExtensions/Properties/AssemblyInfo.cs
+++ HtmlShouldExtensions/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HtmlShouldExtensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Extension methods, in the style of 'Should', that allow developers to make assertions about HTML fragments")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Fourth Hospitality")]
 [assembly: AssemblyProduct("HtmlShouldExtensions")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © Fourth Hospitality 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
